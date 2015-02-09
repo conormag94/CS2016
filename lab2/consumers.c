b5c74aad889edbd949f232fbe0f224d62e2a9bdc
conor <conormag94@gmail.com>
2015-02-09 23:40:38 +0000

    Lab 2 initial file + dir

---
+++ lab2/consumers.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// Comment
+
+int main(){
+    int i = 0;
+    char ** string = "Hello";
+    
+    while(i != NULL){
+        printf("%c", string[i]);
+    }
+    return 0;
+}
